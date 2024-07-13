3b2eb147d05398507e2631fe8b2c02f8061a6b4f
gitcrusher <aayushguptasoni@gmail.com>
2024-07-13 20:16:48 +0530

    third commit

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const app = express();
 const http = require("http");
 
 const socketio = require("socket.io");
-
 const server = http.createServer(app);
-
 const io = socketio(server);
+
+app.set("view engine","ejs");
+app.set(express.static(path.join(__dirname, "public"))); // this coe is written so that we could write vanila css and all etc.
 
 app.get("/",function (req ,res ){
     res.send("hey");
